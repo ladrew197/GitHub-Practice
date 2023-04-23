5ee08a16c1369c4c153fee08d159f13c013d2504
ladrew197 <ladrew@gmail.com>
2023-04-22 20:14:47 -0600

    Initial Commit

---
+++ GitHub Practice/GitHub_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_PracticeApp.swift
+//  GitHub Practice
+//
+//  Created by Lauren Drew on 4/22/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
